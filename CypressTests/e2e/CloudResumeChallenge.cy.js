62eabec5eb17701aa12f62e1b633ffafa7588c41
Matthew Ivancic <mivancic@drwholdings.com>
2022-10-12 12:22:46 -0500

    adding Cypress tests folder

---
+++ CypressTests/e2e/CloudResumeChallenge.cy.js
@@ -0,0 +1,19 @@
+describe ('CRC Viewcount API testing' , () => {
+    it('verify request returns JSON', () => {
+        cy.request('POST', 'https://u2l16jlkki.execute-api.us-east-2.amazonaws.com/prod/CloudResumeChallenge').its('headers').its('content-type').should('include', 'application/json')
+    })
+    it('verify request returns correct code - 200', () => {
+        cy.request('POST', 'https://u2l16jlkki.execute-api.us-east-2.amazonaws.com/prod/CloudResumeChallenge').then((response) => {
+            expect(response.status).to.equal(200)
+        })
+    })
+    it('confirm returned body increments appropriately when POSTed to', () => {
+        let visitors
+        cy.request('POST', 'https://u2l16jlkki.execute-api.us-east-2.amazonaws.com/prod/CloudResumeChallenge').then((response) => {
+            visitors = response
+        })
+        cy.request('POST', 'https://u2l16jlkki.execute-api.us-east-2.amazonaws.com/prod/CloudResumeChallenge').then((response) => {
+            expect(Number(response))==(Number(visitors)+1)
+        })
+    })
+})
