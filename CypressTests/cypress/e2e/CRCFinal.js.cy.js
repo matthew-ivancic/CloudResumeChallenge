68f4bd21bd64d34935a6b10fd0657f09efdf72d9
BobaFettyW4p <111187499+BobaFettyW4p@users.noreply.github.com>
2022-10-12 11:36:34 -0500

    Update CRCFinal.js.cy.js
    updated api

--- CypressTests/cypress/e2e/CRCFinal.js.cy.js
+++ CypressTests/cypress/e2e/CRCFinal.js.cy.js
@@ -1,18 +1,18 @@
 describe ('CRC Viewcount API testing' , () => {
     it('verify request returns JSON', () => {
-        cy.request('POST', 'https://a8h4km0nbk.execute-api.us-east-1.amazonaws.com/Production').its('headers').its('content-type').should('include', 'application/json')
+        cy.request('POST', 'https://u2l16jlkki.execute-api.us-east-2.amazonaws.com/prod/CloudResumeChallenge').its('headers').its('content-type').should('include', 'application/json')
     })
     it('verify request returns correct code - 200', () => {
-        cy.request('POST', 'https://a8h4km0nbk.execute-api.us-east-1.amazonaws.com/Production').then((response) => {
+        cy.request('POST', 'https://u2l16jlkki.execute-api.us-east-2.amazonaws.com/prod/CloudResumeChallenge').then((response) => {
             expect(response.status).to.equal(200)
         })
     })
     it('confirm returned body increments appropriately when POSTed to', () => {
         let visitors
-        cy.request('POST', 'https://a8h4km0nbk.execute-api.us-east-1.amazonaws.com/Production').then((response) => {
+        cy.request('POST', 'https://u2l16jlkki.execute-api.us-east-2.amazonaws.com/prod/CloudResumeChallenge').then((response) => {
             visitors = response.body
         })
-        cy.request('POST', 'https://a8h4km0nbk.execute-api.us-east-1.amazonaws.com/Production').then((response) => {
+        cy.request('POST', 'https://u2l16jlkki.execute-api.us-east-2.amazonaws.com/prod/CloudResumeChallenge').then((response) => {
             expect(Number(response.body))==(Number(visitors)+1)
         })
     })
